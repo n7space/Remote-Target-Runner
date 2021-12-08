c96a2ae2b6f06678af5c50d8b67a898f9d7ff558
jrachucki <jrachucki@n7space.com>
2021-12-08 10:54:21 +0100

    Changed method of parsing arguments

--- runSamV71Binary.py
+++ runSamV71Binary.py
@@ -2,9 +2,14 @@
 
 import os
 import sys
+import getopt
 
 import gdb_runner
 from libs.GdbServerInvoker import GdbServerInvoker
+
+vConsole = ''
+vUart4 = ''
+configPath = 'Config/taste.cfg'
 
 try :
     binary = sys.argv[1]
@@ -12,23 +17,19 @@ except:
     print("Error! SamV71 binary path was not passed to the script!")
     quit()
 
-try : 
-    vConsole = sys.argv[2]
-except:
-    print("Using default console virtual tty: (/tmp/vConsole) ")
-    vConsole = "vConsole"
+try:
+    opts, args = getopt.getopt(sys.argv[2:],"c:v:u:",["config=","vconsole=","uart"])
+except getopt.GetoptError:
+    print("Error while parsing arguments.")
+    sys.exit(2)
 
-try :
-    vUart4 = sys.argv[3]
-except:
-    print("Using default uart4 virtual tty: (/tmp/vUart4)")
-    vUart4 = "vUart4"
-
-try: 
-    configPath = sys.argv[4]
-except:
-    print("Using default config: Config/taste.cfg")
-    configPath ="Config/taste.cfg"
+for opt, arg in opts:
+    if opt in ("-c", "--config"):
+        configPath = arg
+    elif opt in ("-v", "--vConsole"):
+        vConsole = arg
+    elif opt in ("-u", "--uart"):
+        vUart4 = arg
 
 gdbRunner = gdb_runner.gdb_runner(configPath, vConsole, vUart4)
 gdbRunner.startOnGdb(binary)
